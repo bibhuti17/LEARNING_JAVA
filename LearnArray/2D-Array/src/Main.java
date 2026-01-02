62b0f4226b458061fc325e94dad24afc1bee349f
Bibhuti Bhusan Naik <126508100+bibhuti17@users.noreply.github.com>
2026-01-02 23:29:06 +0530

    spiral matrix

---
+++ LearnArray/2D-Array/src/Main.java
@@ -0,0 +1,42 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1,2,3,4},
+                {5,6,7,8},
+                {9,10,11,12},
+                {13,14,15,16},
+                {17,18,19,20}
+        };
+        printSpiral(matrix);
+    }
+
+    public static void printSpiral(int[][] matrix) {
+            int startRow = 0, endRow = matrix.length - 1;
+            int startCol = 0, endCol = matrix[0].length - 1;
+
+            while (startRow <= endRow && startCol <= endCol) {
+                //top
+                for (int j = startCol; j <= endCol; j++) {
+                    System.out.print(matrix[startRow][j] + " ");
+                }
+                //right
+                for (int i = startRow+1 ; i <= endRow; i++) {
+                    System.out.print(matrix[i][endCol] + " ");
+                }
+                //bottom
+                for (int j = endCol - 1; j >= startCol; j--) {
+                    System.out.print(matrix[endRow][j] + " ");
+                }
+                //left
+                for (int i = endRow - 1; i >= startRow+1; i--) {
+                    System.out.print(matrix[i][startCol] + " ");
+                }
+                startRow++;
+                startCol++;
+                endRow--;
+                endCol--;
+            }
+    }
+}
